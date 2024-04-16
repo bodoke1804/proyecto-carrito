53173a2005e48621ac7e1c1e4bd4b6555f1ba707
bodoke1804 <153574891+bodoke1804@users.noreply.github.com>
2024-04-15 18:31:16 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import CartContent from './components/CartContent/CartContent';
+import DataProvider from './components/Context/DataContext';
+
+
+function App() {
+  return (
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<CartContent />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>Potato shop</h1>
+        <p>The best place to find your potatos</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
